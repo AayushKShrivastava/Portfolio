870c1b45aa2355f585c77bb1fbfb3bc4cc222d8b
Aayush Shrivastava <2019002@iiitdmj.ac.in>
2022-03-22 16:41:47 +0530

    added border to work cards

--- src/components/Work.js
+++ src/components/Work.js
@@ -12,7 +12,7 @@ const Work = () => {
         <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
             <div className='pb-8'>
                 <p className='text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600'>Work</p>
-                <p className='py-6'>// Check out some of my recent works</p>
+                <p className='py-6'>-- Check out some of my recent works</p>
             </div>
             <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-6'>
                 <div style={{backgroundImage: `url(${NotesImg})`}} className='border-2 border-pink-600 hover:border-gray-300 shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
