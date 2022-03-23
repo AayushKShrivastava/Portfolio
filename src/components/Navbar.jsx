5728b60e3cfc56abd3f869931e4430e63abe5f1e
Aayush Shrivastava <2019002@iiitdmj.ac.in>
2022-03-23 09:34:43 +0530

    edited state in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ import {Link} from "react-scroll"
 
 const Navbar = () => {
     const [nav, setNav] = React.useState(false)
-    const handleClick = () => setNav(!nav)
+    const handleClick = () => setNav(prevNav => !nav)
   return (
     <div  className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
         <div>
